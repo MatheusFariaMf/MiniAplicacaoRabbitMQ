8554228fa28601bb907c1991daa5766d262cd2a3
MatheusFariaMf <matheus.mfd@hotmail.com>
2025-01-22 19:51:21 -0300

    FinalizaçãoAplicação
    Realização de troca de mensagem entre uma solução "Publisher" e uma solução "Consumer", onde a segunda aplicação consome a mensagem publicada na fila do RabbitMQ pela primeira aplicação.

---
+++ Consumer/Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+using (var connection = factory.CreateConnection())
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "saudacao_1",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        Console.WriteLine($" antes 1  111");
+        consumer.Received += (model, ea) =>
+        {
+            Console.WriteLine($" antes");
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [X] Recebida: {message}");
+        };
+        Console.WriteLine($" depois");
+        channel.BasicConsume(queue: "saudacao_1", autoAck: true, consumer: consumer);
+
+        Console.ReadLine();
+    }
